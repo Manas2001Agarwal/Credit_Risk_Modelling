053e7626325f6b0ed04222fd82085e1f99e01cb4
Manas Agarwal <manasmrt10@gmail.com>
2024-09-07 21:05:49 +0530

    exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+import logging
+
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -9,8 +11,17 @@ def error_message_detail(error,error_detail:sys):
     
 class CustomException(Exception):
     def __init__(self,error_mesaage,error_detail:sys):
-        super.__init__(error_mesaage)
+        super().__init__(error_mesaage)
         self.error_message = error_message_detail(error_mesaage,error_detail=error_detail)
         
     def __str__(self):
         return self.error_message
+    
+    
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
+            
